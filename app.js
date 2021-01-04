e84206904919a4483fad2a0cc0d4893cbcbbbaf9
antonddemin24 <76960781+antonddemin24@users.noreply.github.com>
2021-01-04 18:02:42 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+$(function (){
+
+    let header = $("#header");
+    let track = $("#company");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $(".nav");
+    let navToggle = $("#navToggle");
+
+    ckeckScroll (scrollPos, introH);
+
+    $(window).on("scroll", function () {
+        scrollPos = $(this).scrollTop();
+
+        ckeckScroll (scrollPos, introH);
+    });
+
+    function ckeckScroll (scrollPos, introH) {
+        if (scrollPos > 50) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 90
+        });
+    });
+
+
+    $("#navToggle").on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+
+    });
+
+
+
+});
